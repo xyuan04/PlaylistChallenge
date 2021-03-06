c2e0d6133b6bd786ef989d1eeaef69491c1d8c6a
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-06 11:25:15 -0500

    completed lab and passed all tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,28 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int clickUp = 0;
+        int clickDown = 0;
+
+        if(startIndex == 0 && this.playList[this.playList.length-1].equals(selection)) {
+            return 1;
+        }
+
+        for(int i = startIndex; i < this.playList.length; i++) {
+            if(!this.playList[i].equals(selection)) {
+                clickDown++;
+            } else break;
+        }
+
+        for(int i = startIndex; i >= 0; i--) {
+            if(!this.playList[i].equals(selection)) {
+                clickUp++;
+            } else break;
+        }
+
+        if (clickDown < clickUp) {
+            return clickDown;
+        } else return clickUp;
     }
+
 }
